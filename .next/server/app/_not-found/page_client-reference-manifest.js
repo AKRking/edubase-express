72b03646fad35347d8dd1c9aacd55d775df98870
djbox9 <42683606+djbox9@users.noreply.github.com>
2025-09-05 01:03:59 +0600

    Delete .next directory

--- .next/server/app/_not-found/page_client-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-globalThis.__RSC_MANIFEST=(globalThis.__RSC_MANIFEST||{});globalThis.__RSC_MANIFEST["/_not-found/page"]={"moduleLoading":{"prefix":"/_next/"},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"clientModules":{},"entryCSSFiles":{"/home/project/src/":[],"/home/project/src/app/_not-found/page":[]},"rscModuleMapping":{},"edgeRscModuleMapping":{}}
